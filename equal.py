6c020156d77a13c46abf7dc42ba434bb7ac1cced
Safiyya A <146670910+lanternofsea@users.noreply.github.com>
2023-10-27 19:27:23 -0400

    Add files via upload

---
+++ equal.py
@@ -0,0 +1,11 @@
+# Your code below this line
+def are_equal(str1, str2):
+
+
+
+
+#Testing the function
+print(are_equal("Apples", "Oranges"))
+print(are_equal("0101", "1010"))
+
+
---
+++ equal_complete.py
@@ -0,0 +1,14 @@
+# Your code below this line
+def are_equal(str1, str2):
+    """checks if the 2 input strings are equal"""
+    
+    if str1 == str2:
+        return True
+    else:
+        return False
+    
+#Testing the function
+print(are_equal("Apples", "Oranges"))
+print(are_equal("0101", "1010"))
+
+
